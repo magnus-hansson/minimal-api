b3cf916ef200234e41c6d6528e8301477ec8866f
Magnus Hansson <magnus-hansson@users.noreply.github.com>
2022-05-31 19:38:47 +0000

    cleaning lady

--- minimalapi/Data/AppDbContext.cs
+++ minimalapi/Data/AppDbContext.cs
@@ -4,11 +4,7 @@ namespace minimalapi.data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-
-        }
-
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
         public DbSet<Command> Commands => Set<Command>();
     }
 }
